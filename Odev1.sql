3b8ec0917ef8b8ced8d83fa6d1e157f14480a84d
derdem7 <derdemm7@gmail.com>
2022-02-26 19:38:14 +0300

    Odev 2 eklendi

--- Odev1.sql
+++ Odev1.sql
@@ -1,6 +1,10 @@
---PatikaDev SQL Modülü Ödev 1
+-- PatikaDev SQL Modülü Ödev 1
 SELECT title,description FROM film;
+--1
 SELECT * FROM film WHERE length>60 AND length<75;
+--2
 SELECT * FROM film WHERE rental_rate=0.99 AND (replacement_cost=12.5 OR replacement_cost=28.99);
+--3
 SELECT last_name FROM customer WHERE first_name='Mary';
+--4
 SELECT * FROM film WHERE NOT length>50 AND (rental_rate=2.99 OR rental_rate=4.99);
